bb49943f6fe36f7973792d4276b5088552017233
Tuncay Arda AYDIN <tncyard@gmail.com>
2025-08-22 03:08:57 +0300

    cylinder tracing fully finished

--- src/render/trace_ray.c
+++ src/render/trace_ray.c
@@ -5,9 +5,7 @@
 #include "color.h"
 #include "libft.h"
 
-
-
-color	trace_ray(scene *sc, ray r)
+typedef struct	s_trace_var
 {
 	plane		*pl;
 	sphere		*sp;
@@ -16,112 +14,132 @@ color	trace_ray(scene *sc, ray r)
 	ent_type	type;
 	material	mat;
 
-	size_t	i;
+	size_t		i;
 	int		closest_i;
 	float	t;
 	float	closest_t;
-
-	i = 0;
-	type = ENT_UNKNOWN;
-	ft_memset(&mat, 0, sizeof(material));
-	closest_i = -1;
-	closest_t = INFINITY;
-	while (i < sc->ent_count)
-	{
-		if ((sc->ents[i].type == ENT_SPHERE && hit_sphere(sc->ents[i].ent, r, &t) && t < closest_t)
-			|| (sc->ents[i].type == ENT_CYLINDER && hit_cylinder(sc->ents[i].ent, r, &t) && t < closest_t)
-			|| (sc->ents[i].type == ENT_CIRCLE && hit_circle(sc->ents[i].ent, r, &t) && t < closest_t)
-			|| (sc->ents[i].type == ENT_PLANE && hit_plane(sc->ents[i].ent, r, &t) && t < closest_t))
-		{
-				closest_t = t;
-				closest_i = i;
-				type = sc->ents[i].type;
-		}
-		i++;
-	}
-	if (closest_i == -1)
-		return (color_scale(sc->ambient.color, sc->ambient.ratio));
 
 	vec3	hit_p;
 	vec3	normal;
 	vec3	l_dir;
 	color	base;
 
-	base = COLOR_INIT;
-	normal = VECTOR_INIT;
-	hit_p = vec_add(r.origin, vec_scale(r.direction, closest_t));
-	if (type == ENT_SPHERE)
-	{
-		sp = (sphere *)sc->ents[closest_i].ent;
-		normal = vec_norm(vec_sub(hit_p, sp->pos));
-		base = sp->color;
-		mat = sp->mat;
-	}
-	else if (type == ENT_PLANE)
-	{
-		pl = (plane *)sc->ents[closest_i].ent;
-		normal = vec_norm(pl->axis);
-		if (vec_dot(normal, vec_scale(r.direction, -1.0f)) < 0.0f)
-			normal = vec_scale(normal, -1.0f);
-		base = pl->color;
-		mat = pl->mat;
-	}
-	else if (type == ENT_CYLINDER)
-	{
-		cy = (cylinder *)sc->ents[closest_i].ent;
-		normal = vec_norm(vec_sub(hit_p, cy->pos));
-		base = cy->color;
-		mat = cy->mat;
-	}
-	else if (type == ENT_CIRCLE)
-	{
-		c = (circle *)sc->ents[closest_i].ent;
-		normal = vec_norm(c->axis);
-		base = c->color;
-		mat = c->mat;
-	}
-
-
 	color	diffuse;
 	color	ambient;
 	color	total_diffuse;
 	size_t	li;
 	float	diffuse_int;
-	li = 0;
-	total_diffuse = COLOR_INIT;
-	ambient = color_scale(color_modul(base, sc->ambient.color), sc->ambient.ratio);
-	while (li < sc->light_count)
-	{
-		l_dir = vec_norm(vec_sub(sc->lights[li].pos, hit_p));
-		if (type == ENT_CYLINDER)
-		{
-			ray	l_ray;
-			l_ray.origin = vec_add(hit_p, vec_scale(normal, 1e-3f));
-			l_ray.direction = l_dir;
 
-			if (hit_circle(&cy->bottom, l_ray, &t) || hit_circle(&cy->top, l_ray, &t))
-			{
-				li++;
-				continue;
-			}
+	color	specular;
+	color	total_specular;
+}				trace_var;
+
+
+bool	get_closest(scene *sc, trace_var	*var, ray r)
+{
+	var->i = 0;
+	var->type = ENT_UNKNOWN;
+	var->closest_i = -1;
+	var->closest_t = INFINITY;
+	while (var->i < sc->ent_count)
+	{
+		if ((sc->ents[var->i].type == ENT_SPHERE && hit_sphere(sc->ents[var->i].ent, r, &var->t) && var->t < var->closest_t)
+			|| (sc->ents[var->i].type == ENT_CYLINDER && hit_cylinder(sc->ents[var->i].ent, r, &var->t) && var->t < var->closest_t)
+			|| (sc->ents[var->i].type == ENT_CIRCLE && hit_circle(sc->ents[var->i].ent, r, &var->t) && var->t < var->closest_t)
+			|| (sc->ents[var->i].type == ENT_PLANE && hit_plane(sc->ents[var->i].ent, r, &var->t) && var->t < var->closest_t))
+		{
+				var->closest_t = var->t;
+				var->closest_i = var->i;
+				var->type = sc->ents[var->i].type;
 		}
-		diffuse_int = fmaxf(0.0f, vec_dot(normal, l_dir)) * sc->lights[li].ratio;
-		diffuse = color_scale(color_modul(base, sc->lights[li].color), diffuse_int);
-		total_diffuse = color_add(total_diffuse, diffuse);
-		li++;
+		var->i++;
 	}
+	if (var->closest_i == -1)
+		return (false);
+	return (true);
+}
 
-
-	li = 0;
-	color	specular = COLOR_INIT;
-	color	total_specular = COLOR_INIT;
-	if (mat.has_spc)
+void	define_closest(scene *sc, trace_var	*var, ray r)
+{
+	var->base = COLOR_INIT;
+	var->normal = VECTOR_INIT;
+	var->hit_p = vec_add(r.origin, vec_scale(r.direction, var->closest_t));
+	if (var->type == ENT_SPHERE)
 	{
-		while (li < sc->light_count)
+		var->sp = (sphere *)sc->ents[var->closest_i].ent;
+		var->normal = vec_norm(vec_sub(var->hit_p, var->sp->pos));
+		var->base = var->sp->color;
+		var->mat = var->sp->mat;
+	}
+	else if (var->type == ENT_PLANE)
+	{
+		var->pl = (plane *)sc->ents[var->closest_i].ent;
+		var->normal = vec_norm(var->pl->axis);
+		if (vec_dot(var->normal, vec_scale(r.direction, -1.0f)) < 0.0f)
+			var->normal = vec_scale(var->normal, -1.0f);
+		var->base = var->pl->color;
+		var->mat = var->pl->mat;
+	}
+	else if (var->type == ENT_CYLINDER)
+	{
+		var->cy = (cylinder *)sc->ents[var->closest_i].ent;
+
+		vec3 hp_rel = vec_sub(var->hit_p, var->cy->pos);
+		float t0 = vec_dot(hp_rel, vec_norm(var->cy->axis));
+		vec3 Q = vec_add(var->cy->pos, vec_scale(vec_norm(var->cy->axis), t0));
+
+		vec3 to_axis = vec_sub(Q, var->hit_p);
+		float s = vec_dot(to_axis, r.direction);
+		bool hit_from_inside = (s < 0.0f);
+
+		vec3 N_out = vec_norm(vec_sub(var->hit_p, Q));
+
+		var->normal = N_out;
+		if (!hit_from_inside)
+			vec_scale(N_out, -1.0f);
+
+		var->base = var->cy->color;
+		var->mat = var->cy->mat;
+	}
+	else if (var->type == ENT_CIRCLE)
+	{
+		var->c = (circle *)sc->ents[var->closest_i].ent;
+		var->normal = vec_norm(var->c->axis);
+		if (vec_dot(var->normal, vec_scale(r.direction, -1.0f)) < 0.0f)
+			var->normal = vec_scale(var->normal, -1.0f);
+		var->base = var->c->color;
+		var->mat = var->c->mat;
+	}
+}
+
+void	compute_diffuse(scene *sc, trace_var	*var, ray r)
+{
+	(void)r;
+	var->li = 0;
+	var->total_diffuse = COLOR_INIT;
+	var->ambient = color_scale(color_modul(var->base, sc->ambient.color), sc->ambient.ratio);
+	while (var->li < sc->light_count)
+	{
+		var->l_dir = vec_norm(vec_sub(sc->lights[var->li].pos, var->hit_p));
+		var->diffuse_int = fmaxf(0.0f, vec_dot(var->normal, var->l_dir)) * sc->lights[var->li].ratio;
+		var->diffuse = color_scale(color_modul(var->base, sc->lights[var->li].color), var->diffuse_int);
+		var->total_diffuse = color_add(var->total_diffuse, var->diffuse);
+		var->li++;
+	}
+}
+
+void	compute_specular(scene *sc, trace_var	*var, ray r)
+{
+	var->li = 0;
+	var->specular = COLOR_INIT;
+	var->total_specular = COLOR_INIT;
+	if (var->mat.has_spc)
+	{
+		while (var->li < sc->light_count)
 		{
-			specular = COLOR_INIT;
-			vec3	N = normal;
-			vec3	L = vec_norm(vec_sub(sc->lights[li].pos, hit_p));
+			var->specular = COLOR_INIT;
+			vec3	N = var->normal;
+			vec3	L = vec_norm(vec_sub(sc->lights[var->li].pos, var->hit_p));
 			vec3	V = vec_norm(vec_scale(r.direction, -1.0f));
 			float	NL = vec_dot(N, L);
 			vec3	R = vec_norm(vec_sub(vec_scale(N, 2.0f * NL), L));
@@ -129,14 +147,26 @@ color	trace_ray(scene *sc, ray r)
 			float	spec_term;
 			if (NL > 0)
 			{
-				spec_term = powf(fmax(0, vec_dot(R, V)), mat.shininess);
-				specular_int = mat.specular_strength * spec_term * sc->lights[li].ratio;
-				specular = color_scale(sc->lights[li].color, specular_int);
-				total_specular = color_add(total_specular, specular);
+				spec_term = powf(fmax(0, vec_dot(R, V)), var->mat.shininess);
+				specular_int = var->mat.specular_strength * spec_term * sc->lights[var->li].ratio;
+				var->specular = color_scale(sc->lights[var->li].color, specular_int);
+				var->total_specular = color_add(var->total_specular, var->specular);
 			}
-			li++;
+			var->li++;
 		}	
 	}
+}
 
-	return (color_add(ambient, color_add(total_diffuse, total_specular)));
+color	trace_ray(scene *sc, ray r)
+{
+	trace_var var;
+
+	ft_memset(&var, 0, sizeof(trace_var));
+	if (!get_closest(sc, &var, r))
+		return (color_scale(sc->ambient.color, sc->ambient.ratio));
+	define_closest(sc , &var, r);
+	compute_diffuse(sc, &var, r);
+	compute_specular(sc, &var, r);
+
+	return (color_add(var.ambient, color_add(var.total_diffuse, var.total_specular)));
 }
