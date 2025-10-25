3aeb1e2b40365d06ec0f171446fec858bd05457e
Tuncay Arda AYDIN <tncyard@gmail.com>
2025-10-26 00:08:02 +0300

    xpm world_map

--- src/render/compute_texture.c
+++ src/render/compute_texture.c
@@ -9,19 +9,17 @@ static t_color get_texture_sphere(t_trace_var *var)
 	var->u = 1.0f - var->u;
 	var->v = 0.5f - asinf(var->local.y) / M_PI;
 
-	int x = (int)floorf(var->u * (float)var->mat.tex_width);
-    int y = (int)floorf(var->v * (float)var->mat.tex_height);
-    if (x < 0) x = 0; if (x >= var->mat.tex_width) x = var->mat.tex_width - 1;
-    if (y < 0) y = 0; if (y >= var->mat.tex_height) y = var->mat.tex_height - 1;
+	var->iu = (int)floorf(var->u * (float)var->mat.tex_width);
+	var->iv = (int)floorf(var->v * (float)var->mat.tex_height);
 
-    int bpp_bytes = var->mat.tex_bpp / 8;
-    const unsigned char *pix = (const unsigned char*)(var->mat.tex_addr + y * var->mat.tex_line_len + x * bpp_bytes);
+	int bpp_bytes = var->mat.tex_bpp / 8;
+	const unsigned char *pix = (const unsigned char*)(var->mat.tex_addr + var->iv * var->mat.tex_line_len + var->iu * bpp_bytes);
 
-    t_color c;
-    c.b = pix[0];
-    c.g = pix[1];
-    c.r = pix[2];
-    return c;
+	t_color c;
+	c.b = pix[0];
+	c.g = pix[1];
+	c.r = pix[2];
+	return c;
 }
 
 static t_color get_texture_plane(t_trace_var *var)
